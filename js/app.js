6b15a7ad20980ecbf24c23770f7b2c85926b5df8
Laydemir <voldvakoff@gmail.com>
2022-06-20 00:15:39 +0300

    функциональность модальных окон

--- js/app.js
+++ js/app.js
@@ -1,4 +1,8 @@
 $(function () {
+
+	/* filter
+	=====================*/
+
 	let filter = $("[data-filter]");
 	filter.on("click", function (event) {
 		event.preventDefault();
@@ -18,4 +22,62 @@ $(function () {
 			});
 		}
 	});
+
+	/* modal
+	===========================*/
+
+	const modalCall = $("[data-modal]");
+	const modalClose = $("[data-close]");
+
+	modalCall.on("click", function (event) {
+		event.preventDefault();
+
+		let $this = $(this);
+		let modalId = $this.data('modal');
+
+		$(modalId).addClass('show');
+		$("body").addClass('no-scroll');
+
+		setTimeout(function () {
+			$(modalId).find(".modal__dialog").css({
+				transform: "scale(1)"
+			});
+		}, 200);
+	});
+
+	modalClose.on("click", function (event) {
+		event.preventDefault();
+
+		let $this = $(this);
+		let modalParent = $this.parents('.modal');
+
+		modalParent.find(".modal__dialog").css({
+			transform: "scale(0)"
+		});
+
+		setTimeout(function () {
+			modalParent.removeClass('show');
+			$("body").removeClass('no-scroll');
+		}, 200);
+	});
+
+	$(".modal").on("click", function (event) {
+		let $this = $(this);
+		$this.find(".modal__dialog").css({
+			transform: "scale(0)"
+		});
+
+		setTimeout(function () {
+			$this.removeClass('show');
+			$("body").removeClass('no-scroll');
+		}, 200);
+	});
+
+	$(".modal__dialog").on("click", function (event) {
+		event.stopPropagation();
+	});
+
+
+
+
 });
